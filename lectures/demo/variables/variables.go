6970bb270069d7b5c9c45c5bd2f857b3133013da
Dubjay18 <jejeniyi7@gmail.com>
2024-01-29 18:34:03 +0100

    demo variables

--- lectures/demo/variables/variables.go
+++ lectures/demo/variables/variables.go
@@ -3,4 +3,30 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Jayson"
+	fmt.Println("my name is", myName)
+
+	var name string = "Dorthy"
+	fmt.Println("name =", name)
+
+	userName := "admin"
+	fmt.Println("username =", userName)
+
+	var sum int
+	fmt.Println("The sum is", sum)
+
+	part1, other := 1, 5
+	fmt.Println("part 1 is", part1, "other is", other)
+	part2 := 9
+	sum = part1 + part2
+	fmt.Println("sum =", sum)
+
+	var (
+		lessonName = "variables"
+		lessonType = "Demo"
+	)
+
+	fmt.Println("lessonName", lessonName)
+	fmt.Println("lessonType", lessonType)
+
 }
