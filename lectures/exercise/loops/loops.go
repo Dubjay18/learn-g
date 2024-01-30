616006bd999adc120799398cabbe3b939afffca0
Dubjay18 <jejeniyi7@gmail.com>
2024-01-31 00:42:17 +0100

    exercise loops

--- lectures/exercise/loops/loops.go
+++ lectures/exercise/loops/loops.go
@@ -15,4 +15,24 @@ package main
 import "fmt"
 
 func main() {
+	//* Print integers 1 to 50, except:
+	for i := 1; i <= 50; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+
+		}
+		//  - Print "Fizz" if the integer is divisible by 3
+	}
+
+	//  - Print "Buzz" if the integer is divisible by 5
+	//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+	//
 }
