597e66a0254c87621c60c581d2b63c401aa9288d
Dubjay18 <jejeniyi7@gmail.com>
2024-02-04 18:59:15 +0100

    exercise variadics

--- lectures/demo/variadics/variadics.go
+++ lectures/demo/variadics/variadics.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(nums ...int) int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
 
+func main() {
+	a := []int{1, 2, 3, 4, 5}
+	b := []int{6, 7, 8, 9, 10}
+
+	all := append(a, b...)
+
+	answer := sum(all...)
+	fmt.Println(answer)
 }
