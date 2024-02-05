7e4dda646cd337fbc4906861ff47dab112bb34b3
Dubjay18 <jejeniyi7@gmail.com>
2024-02-05 14:52:40 +0100

    exercise test

--- lectures/exercise/errors/errors.go
+++ lectures/exercise/errors/errors.go
@@ -15,3 +15,54 @@
 //* Use the `errors` package to generate errors
 
 package timeparse
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type Time struct {
+	hours, minute, second int
+}
+
+type TimeParserError struct {
+	message string
+	input   string
+}
+
+func (t *TimeParserError) Error() string {
+	return fmt.Sprintf("%v : %v", t.message, t.input)
+
+}
+
+func ParseTime(input string) (Time, error) {
+	components := strings.Split(input, ":")
+	if len(components) != 3 {
+		return Time{}, &TimeParserError{"invalid time string", input}
+	} else {
+		hour, err := strconv.Atoi(components[0])
+		if err != nil {
+			return Time{}, &TimeParserError{fmt.Sprintf("invalid hour: %v", err), input}
+		}
+		minute, err := strconv.Atoi(components[1])
+		if err != nil {
+			return Time{}, &TimeParserError{fmt.Sprintf("invalid minute: %v", err), input}
+		}
+		second, err := strconv.Atoi(components[2])
+		if err != nil {
+			return Time{}, &TimeParserError{fmt.Sprintf("invalid second: %v", err), input}
+		}
+		if hour < 0 || hour > 23 {
+			return Time{}, &TimeParserError{"hour out of range", input}
+		}
+		if minute < 0 || minute > 59 {
+			return Time{}, &TimeParserError{"minute out of range", input}
+		}
+		if second < 0 || second > 59 {
+			return Time{}, &TimeParserError{"second out of range", input}
+		}
+
+		return Time{hour, minute, second}, nil
+	}
+}
---
+++ lectures/exercise/errors/errors_test.go
@@ -0,0 +1,30 @@
+package timeparse
+
+import "testing"
+
+func TestParseTime(t *testing.T) {
+	table := []struct {
+		time string
+		ok   bool
+	}{
+		{"14:07:33", true},
+		{"14:07", false},
+		{"14:07:60", false},
+		{"14:60:33", false},
+		{"24:07:33", false},
+		{"14:07:33", true},
+		{"-14:07:33", false},
+		{"14:-07:33", false},
+		{"14:07:-33", false},
+		{"14:07:33", true},
+		{"14:07:33", true},
+		{"14:07:33", true},
+		{"14:07:33", true},
+	}
+	for _, test := range table {
+		_, err := ParseTime(test.time)
+		if test.ok && err != nil {
+			t.Errorf("%v: unexpected error: %v", test.time, err)
+		}
+	}
+}
