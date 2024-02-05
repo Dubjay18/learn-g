dc65039dbc14d99b06897a0a5e8251905906a7da
Dubjay18 <jejeniyi7@gmail.com>
2024-02-05 13:30:52 +0100

    interfaces

--- lectures/demo/interfaces/interfaces.go
+++ lectures/demo/interfaces/interfaces.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+type Preparer interface {
+	PrepareDish()
+}
+type Chicken string
+type Salad string
+
+func (c Chicken) PrepareDish() {
+	fmt.Println("cook chicken")
+}
+func (s Salad) PrepareDish() {
+	fmt.Println("toss salad")
+}
+
+func prepareDishes(dishes []Preparer) {
+	for i := 0; i < len(dishes); i++ {
+		dish := dishes[i]
+		fmt.Printf("--Dish %v--\n", dish)
+		dish.PrepareDish()
+	}
+	fmt.Println()
+}
 func main() {
+	dishes := []Preparer{Chicken("chicken wings"), Salad("garden salad")}
+	prepareDishes(dishes)
+
+	dishes = append(dishes, Chicken("chicken parm"))
+	prepareDishes(dishes)
+
+	dishes = append(dishes, Salad("caesar salad"))
+
+	prepareDishes(dishes)
 
 }
--- lectures/exercise/interfaces/interfaces.go
+++ lectures/exercise/interfaces/interfaces.go
@@ -21,6 +21,68 @@ package main
 
 import "fmt"
 
+const (
+	SmallLift = iota
+	StandardLift
+	LargeLift
+)
+
+type Lift int
+type Truck string
+type Car string
+type Motorcycle string
+type model string
+
+type LiftHandler interface {
+	HandleLift() Lift
+}
+
+func (t Truck) HandleLift() Lift {
+	return LargeLift
+}
+func (c Car) HandleLift() Lift {
+	return StandardLift
+}
+func (m Motorcycle) HandleLift() Lift {
+	return SmallLift
+}
+
+func handleLifts(vehicles []LiftHandler) {
+	for i := 0; i < len(vehicles); i++ {
+		vehicle := vehicles[i]
+		lift := vehicle.HandleLift()
+		switch lift {
+		case SmallLift:
+			fmt.Println("Motorcycle on small lift")
+		case StandardLift:
+			fmt.Println("Car on standard lift")
+		case LargeLift:
+			fmt.Println("Truck on large lift")
+		}
+	}
+}
+
+func (t Truck) String() {
+	fmt.Sprintf("Truck: %V/ ", string(t))
+}
+func (c Car) String() {
+	fmt.Sprintf("Car: %V/ ", string(c))
+}
+
+func (m Motorcycle) String() {
+	fmt.Sprintf("Motorcycle: %V/ ", string(m))
+}
+
 func main() {
+	car := Car("Toyota")
+	truck := Truck("Ford")
+	motorcycle := Motorcycle("Harley")
+
+	vehicles := []LiftHandler{car, truck, motorcycle}
+	handleLifts(vehicles)
+
+	car.String()
+	truck.String()
+	motorcycle.String()
 
 }
