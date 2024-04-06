2cf94c40b17d2c26bdfb25b80404027d46b8129b
Dubjay18 <jejeniyi7@gmail.com>
2024-04-06 15:16:10 +0100

    demo mutexes

--- lectures/demo/mutexes/mutexes.go
+++ lectures/demo/mutexes/mutexes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,6 +11,35 @@ func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 }
 
+type Hits struct {
+	count int
+	sync.Mutex
+}
+
+func serve(wg *sync.WaitGroup, h *Hits, iteration int) {
+	wait()
+	h.Lock()
+	defer h.Unlock()
+	defer wg.Done()
+	h.count++
+	fmt.Println("serving", iteration)
+}
+
 func main() {
 	rand.New(rand.NewSource(time.Now().UnixNano()))
+	var wg sync.WaitGroup
+	hitCounter := Hits{}
+	for i := 0; i < 20; i++ {
+		iteration := i
+		wg.Add(1)
+		go serve(&wg, &hitCounter, iteration)
+	}
+
+	fmt.Printf("waiting for goroutines..\n\n")
+	wg.Wait()
+	hitCounter.Lock()
+	totalHits := hitCounter.count
+	hitCounter.Unlock()
+
+	fmt.Println(totalHits)
 }
